5d4d6958e7c0bc8058706959e34ffe7b13ccbd0b
mi_farhan <mistershaikh397@gmail.com>
2026-09-21 18:12:01 +0530

    Revert "Add sound notifications to admin panel"
    This reverts commit c43a832fdeca7024dd59286d87a01ab80bbec28a.

--- app/admin/(dashboard)/layout.tsx
+++ app/admin/(dashboard)/layout.tsx
@@ -1,5 +1,4 @@
 import { AdminSidebar } from "@/components/admin/AdminSidebar";
-import { AdminNotifications } from "@/components/admin/AdminNotifications";
 
 export default function AdminDashboardLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,9 +6,6 @@ export default function AdminDashboardLayout({ children }: { children: React.Rea
       <AdminSidebar />
       {/* mt-14 md:mt-0: offset for mobile fixed top bar; desktop sidebar is in normal flow */}
       <main className="flex-1 mt-14 md:mt-0 p-4 md:p-8 lg:p-10 min-w-0">
-        <div className="flex justify-end mb-2">
-          <AdminNotifications />
-        </div>
         {children}
       </main>
     </div>
